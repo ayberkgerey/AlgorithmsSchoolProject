d5739034217c683988a39aa19e9ad65085149cd5
Ayberk Gerey <ayberkgerey@gmail.com>
2020-03-10 20:36:45 +0300

    Add files via upload

--- parsejson.py
+++ parsejson.py
@@ -3,9 +3,5 @@ import json
 with open ('coordinates.json') as f:
     data = json.load(f)
 
-lenght_of_products = (len(data["features"]["SymbolID"]["rings"]))
-
-for x in range(length_of_back):
-
- print((data["features"]["SymbolID"][x]["rings"]))
+print(data)
 
