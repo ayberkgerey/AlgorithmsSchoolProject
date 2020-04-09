54f1472c9bf4ddda131f5f98cae62f1926f3f658
Ayberk Gerey <ayberkgerey@gmail.com>
2020-04-09 14:07:52 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,60 +3,75 @@
 #include <time.h>
 #include <ctype.h>
 
+void gameLoop();
 int rollDice();
-
-void decideScore(int sum);
-
-void longGame(int savedSum);
+int ask(int currentDice);
 
 int main() {
-    int sum;
-    char decide, lastDecide = 'e';
-
-    while (lastDecide != 'k') {
-        sum = rollDice();
-        decideScore(sum);
-
-        scanf("%c", decide);
-        lastDecide = tolower(decide);
-
-        if (decide == 'a') longGame(sum);
+    char c[2];
+    srand(time(NULL));
+    gameLoop();
+    while (1) {
+        printf("\nYeni oyun oynansin mi (e/E - h/H) ? ");
+        gets(c);
+        if (tolower(c[0]) == 'e')
+            gameLoop();
+        else if (tolower(c[0]) == 'h')
+            break;
+        else {
+            printf("Bilinmeyen komut, cikiliyor.");
+            break;
+        }
     }
-
+    printf("\n");
     system("pause");
     return 0;
 }
 
+void gameLoop() {
+    int currentDice, wantedDice = 0;
+    while (1) {
+        currentDice = rollDice();
+        if(wantedDice == 0 && (currentDice == 11 || currentDice == 7)){
+            printf("Zarlar Atiliyor... %d Kazandiniz",currentDice);
+            break;
+        }
+        if(wantedDice == 0 && (currentDice == 2 || currentDice == 3 || currentDice == 12)){
+            printf("Zarlar Atiliyor... %d Kaybettiniz",currentDice);
+            break;
+        }
+        else if (wantedDice == 0){
+            wantedDice = currentDice;
+            printf("Zarlar Atiliyor... %d Sonuc belirsiz, tekrar zar atilacak. At? (a/A)? ", currentDice);
+            if(!ask(currentDice))
+                break;
+        }
+        else {
+            if (currentDice == wantedDice) {
+                printf("Zarlar Atiliyor... %d Kazandiniz",currentDice);
+                break;
+            } else if (currentDice == 7) {
+                printf("Zarlar Atiliyor... %d Kaybettiniz",currentDice);
+                break;
+            } else {
+                printf("Zarlar Atiliyor... %d Sonuc belirsiz, tekrar zar atilacak. At? (a/A)? ", currentDice);
+                if(!ask(currentDice))
+                    break;
+            }
+        }
+    }
+}
+
+int ask(int currentDice){
+    char c[2];
+    gets(c);
+    if(!(tolower(c[0]) == 'a'))
+        return 0;
+    return 1;
+}
+
 int rollDice() {
-    srand(time(NULL));
     int x = rand() % 6 + 1;
     int y = rand() % 6 + 1;
     return x + y;
-}
-
-void decideScore(int sum) {
-
-    if (sum == 7 || sum == 11) {
-        printf("Zarlar atiliyor... %d kazandiniz\nYeni oyun oynansin mi ? (e/E - h/H) ?", sum);
-    } else if (sum == 2 || sum == 3 || sum == 12) {
-        printf("Zarlar atiliyor... %d kaybettiniz\nYeni oyun oynansin mi ? (e/E - h/H) ?", sum);
-    } else {
-        printf("Zarlar atiliyor... %d Sonuc Belirsiz, tekrar zar atilacak. At(a/A) ?", sum);
-    }
-
-}
-
-void longGame(int savedSum) {
-
-    int currentSum;
-
-    while (savedSum != currentSum) {
-        currentSum = rollDice();
-        printf("Zarlar atiliyor... %d Sonuc Belirsiz, tekrar zar atilacak. At(a/A) ?", currentSum);
-    }
-
-    if (savedSum == currentSum)
-        printf("Zarlar atiliyor... %d Kazandiniz");
-    else
-        printf("CRAPS sona erdi.");
 }
